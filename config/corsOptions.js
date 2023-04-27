33bbf3bed7d5adb26d4fce38bdfee3ad6202a360
Banky <mac@macs-MacBook-Pro.local>
2023-04-27 10:09:35 +0100

    corsOption update

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,6 +1,6 @@
 // Cross Origin Resource Sharing
 const allowedOrigin = [
-  "https://www.yoursite.com",
+  "https://companydb.onrender.com",
   "http://127.0.0.1:5500",
   "http://localhost:3500",
 ];
